b02184a680024d339ab21c067bab487824804876
suresh <surya2fun@gmail.com>
2023-07-21 12:26:23 +0530

    roboshop code new edits

--- cart.sh
+++ cart.sh
@@ -1,5 +1,4 @@
 cp cart.service /etc/systemd/system/cart.service
-
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
@@ -13,3 +12,4 @@ npm install
 systemctl daemon-reload
 systemctl enable cart
 systemctl restart cart
+
