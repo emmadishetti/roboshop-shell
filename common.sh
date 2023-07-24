99d94ff630782f299c2a669618b921627e7185ca
suresh <surya2fun@gmail.com>
2023-07-24 23:02:15 +0530

    roboshop variables

--- common.sh
+++ common.sh
@@ -1,43 +1,43 @@
 log=/tmp/roboshop.log
-echo ">>>>>>>>>>> ${component} Service <<<<<<<<<"
+echo -e  "\e[35m>>>>> ${component} Service <<<<<<<<<\e[0m"
 cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
 
-echo ">>>>>>>>>>> ctreate Mongo Repo  <<<<<<<<<"
+echo -e  "\e[35m>>>>> ctreate Mongo Repo  <<<<<<<<<"
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
 
-echo ">>>>>>>>>>> Download nodejs Repos <<<<<<<<<"
+echo -e  "\e[35m>>>>> Download nodejs Repos <<<<<<<<<"\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 
-echo ">>>>>>>>>>> Install Nodejs <<<<<<<<<"
+echo -e  "\e[35m>>>>> Install Nodejs <<<<<<<<<\e[0m"
 yum install nodejs -y &>>${log}
 
-echo ">>>>>>>>>>> Create Application User <<<<<<<<<"
+echo -e  "\e[35m>>>>>Create Application User <<<<<<<<<\e[0m"
 useradd roboshop &>>${log}
 
-echo ">>>>>>>>>>> Removing exisiting data <<<<<<<<<"
+echo -e  "\e[35m>>>>> Removing exisiting data <<<<<<<<<\e[0m"
 rm -rf /app &>>${log}
 
-echo ">>>>>>>>>>> Creating the Application Directory  <<<<<<<<<"
+echo -e  "\e[35m>>>>> Creating the Application Directory  <<<<<<<<<\e[0m"
 mkdir /app &>>${log}
 
-echo ">>>>>>>>>>> Download Application Content <<<<<<<<<"
+echo -e  "\e[35m>>>>> Download Application Content <<<<<<<<<\e[0m"
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
 
-echo ">>>>>>>>>>> Extract Application Content <<<<<<<<<"
+echo -e  "\e[35m>>>>> Extract Application Content <<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/${component}.zip &>>${log}
 cd /app
 
-echo ">>>>>>>>>>> Download Nodejs Dependencies  <<<<<<<<<"
+echo -e  "\e[35m>>>>> Download Nodejs Dependencies  <<<<<<<<<\e[0m"
 npm install &>>${log}
 
-echo ">>>>>>>>>>> Install MongoDB Client <<<<<<<<<"
+echo -e  "\e[35m>>>>> Install MongoDB Client <<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y &>>${log}
 
-echo ">>>>>>>>>>> Load ${component} Schema <<<<<<<<<"
+echo -e  "\e[35m>>>>> Load ${component} Schema <<<<<<<<<\e[0m"
 mongo --host mongodb.devops7874.online </app/schema/${component}.js &>>${log}
 
-echo ">>>>>>>>>>> Start ${component} Service <<<<<<<<<"
+echo -e  "\e[35m>>>>> Start ${component} Service <<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable ${component}
 systemctl restart ${component} &>>${log}
